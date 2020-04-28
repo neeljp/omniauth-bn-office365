fda9eafbc83afe947e656b1a423c7de9f4913ac1
Neel Peters <njp@riseup.net>
2020-04-28 16:48:10 +0200

    Update office365.rb

--- lib/omniauth/strategies/office365.rb
+++ lib/omniauth/strategies/office365.rb
@@ -15,8 +15,8 @@ module OmniAuth
       # Configure the Azure v2 endpoints
       option  :client_options,
               site:          'https://login.microsoftonline.de',
-              authorize_url: '/common/oauth2/v2.0/authorize',
-              token_url:     '/common/oauth2/v2.0/token'
+              authorize_url: '/common/oauth2/authorize',
+              token_url:     '/common/oauth2/token'
 
       # Send the scope parameter during authorize
       option :authorize_options, [:scope, :hd]
